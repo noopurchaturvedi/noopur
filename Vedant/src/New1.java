4ac6e23094c2267e9190de1a5adfd991a995cd63
noopur.chaturvedi <noopur.chaturvedi@BDC6-DX-87RMX52.dir.svc.accenture.com>
2019-05-10 01:43:09 -0500

    new commit

--- Vedant/src/New1.java
+++ Vedant/src/New1.java
@@ -3,5 +3,7 @@ public class New1 {
 public static void main(String args[]){
 	System.out.println("Hello how are you");
 	System.out.println("How are you");
+	System.out.println("how is t");
+	System.out.println("how is it");
 }
 }
